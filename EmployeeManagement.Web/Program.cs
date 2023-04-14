1f82ddd3e69fa8a61ac770a4355c2d2056e2e52e
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-04-14 17:25:19 +0100

    Removal of redundant links

--- EmployeeManagement.Web/Program.cs
+++ EmployeeManagement.Web/Program.cs
@@ -1,4 +1,3 @@
-using EmployeeManagement.Web.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -13,7 +12,6 @@ namespace EmployeeManagement.Web
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
