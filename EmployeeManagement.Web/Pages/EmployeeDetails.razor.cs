a826ade7b82d6b23dcf741cfc582e4b8894320a5
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-04-15 14:09:11 +0100

    some event handling testing

--- EmployeeManagement.Web/Pages/EmployeeDetails.razor.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetails.razor.cs
@@ -1,12 +1,15 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages;
 
 public partial class EmployeeDetails
 {
-    public Employee Employee { get; set; } = new Employee();
+    public Employee Employee { get; set; }
+
+    protected string Coordinates { get; set; }
 
     [Inject]
     public IEmployeeService EmployeeService { get; set; }
@@ -17,6 +20,11 @@ public partial class EmployeeDetails
     protected async  override Task OnInitializedAsync()
     {
         Employee = await EmployeeService.Get(int.Parse(Id));
+    }
+
+    protected void Mouse_Move(MouseEventArgs e)
+    {
+        Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
     }
 }
 
