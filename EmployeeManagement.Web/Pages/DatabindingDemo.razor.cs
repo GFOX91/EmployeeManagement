22781c4802c1db1dabb713004c259f299bef82ad
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-04-15 14:45:14 +0100

    Playing round with some data bindings

---
+++ EmployeeManagement.Web/Pages/DatabindingDemo.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.Web.Pages;
+
+public partial class DatabindingDemo
+{
+    protected string Name { get; set; } = "Greg";
+    protected string Gender { get; set; } = "Greg";
+    protected string Colour { get; set; } = "background-color:white";
+    protected string Description { get; set; } = string.Empty;
+}
+
